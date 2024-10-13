081a2bb9a69848cf04d0cc0067f4f821351902b6
najmamoh <mnajma179@gmail.com>
2024-10-13 10:25:33 +0300

    change1

--- src/views/Index.js
+++ src/views/Index.js
@@ -5,10 +5,7 @@ import { Link } from "react-router-dom";
 import IndexNavbar from "components/Navbars/IndexNavbar.js";
 import Footer from "components/Footers/Footer.js";
 
-//rrr
-//rrr
-//rrr
-//rrr
+
 
 export default function Index() {
   return (
