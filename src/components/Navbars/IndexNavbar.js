f93328c65406091879439e854ad6f4f54f7ee93e
najmamoh <mnajma179@gmail.com>
2024-10-15 10:48:57 +0300

    change3

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -62,7 +62,7 @@ export default function Navbar(props) {
               </li>  <li className="flex items-end">
                 <a
                   className="hover:text-blueGray-500 text-blueGray-700 px-3 py-4 lg:py-2 flex items-center text-xs uppercase font-bold"
-                  href="#"
+                  href="#Services"
                 >
                   Services
                 </a>
